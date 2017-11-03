0469f48604b16413850bd9dfe5750301351f9fb0
Unknown <keregan.schoenherr@uconn.edu>
2017-11-02 22:52:42 -0400

    // v.01 movement
    basic wind

--- SailRacers/Assets/PlayerController.cs
+++ SailRacers/Assets/PlayerController.cs
@@ -5,23 +5,42 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour {
 	public float speed;
 	public int point;
-
+	public float windSpeed;
+	public int windDir;
+	public float totalSpeed;
+	private int variwind;
+	public GameObject windMarker;
 	// Use this for initialization
 	void Start () {
 		point = 0;
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		
-		transform.Translate(Vector2.up *(-speed) * Time.deltaTime);
+		variwind = Mathf.Abs ((Mathf.RoundToInt(gameObject.transform.localEulerAngles.z) - windDir) %360 )/45;
+		totalSpeed = (speed * windSpeed) - variwind;
+
+		if (totalSpeed <= 1)
+			totalSpeed = 1;
+		transform.Translate(Vector2.up *(-totalSpeed) * Time.deltaTime);
 		if (Input.GetKeyDown (KeyCode.RightArrow)) {
 			point += 1;
 			transform.Rotate (0, 0, -45);
+
 		}
 		else if (Input.GetKeyDown (KeyCode.LeftArrow)) {
 			point -= 1;
 			transform.Rotate (0, 0, 45);
 		}
+		Debug.Log(windMarker.transform.rotation.eulerAngles.z );
+		if (windMarker.transform.rotation.eulerAngles.z < 0) {
+			
+			windMarker.transform.rotation = Quaternion.Euler (0, 0, -windDir);
+		} else {
+			windMarker.transform.rotation = Quaternion.Euler (0, 0, -windDir);
+		}
+	
 	}
 }
